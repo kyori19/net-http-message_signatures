950492eadc4d8301b586c7bd555664dbaf2d4a73
kyori19 <kyori@accelf.net>
2023-10-08 08:44:51 +0900

    Setup RuboCop

--- lib/net/http/message_signatures.rb
+++ lib/net/http/message_signatures.rb
@@ -2,10 +2,13 @@
 
 require 'net/http'
 
-require_relative "message_signatures/version"
+require_relative 'message_signatures/version'
 
 module Net
   class HTTP
+    # A Ruby implementation of HTTP Message Signatures.
+    #
+    # @see {https://httpwg.org/http-extensions/draft-ietf-httpbis-message-signatures.html}
     module MessageSignatures
     end
   end
